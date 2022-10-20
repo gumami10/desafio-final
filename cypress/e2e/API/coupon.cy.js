55b4c1e5851a7ede48b597d1ce5097c87792c340
Eric Groppe Lepore Marques <eric.lepore@hbsis.com.br>
2022-10-20 09:02:24 -0300

    improve API tests

--- cypress/e2e/API/coupon.cy.js
+++ cypress/e2e/API/coupon.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 
 let token;
+const headers = {
+    authorization: 'Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy',
+}
+const url = '/wp-json/wc/v3/coupons'
 
 
 describe('[US-0003] – API de cupons', () => {
@@ -10,14 +14,30 @@ describe('[US-0003] – API de cupons', () => {
     it('Deve listar todos os cupons cadastrado', () => {
         cy.request({
             method: 'GET',
-            headers: {
-                authorization: 'Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy',
-            },
-            url: '/coupons'
+            headers,
+            url
         }).then(res => {
-            cy.log(res)
+            expect(res.status).to.be.eql(200)
+            expect(res.body).length.to.be.above(0)
+        })
+    })
+
+    it('Deve listar todos os cupons cadastrado', () => {
+        const code = `coupon:${Date.now().toString()}`
+        cy.request({
+            method: 'POST',
+            headers,
+            url,
+            body: {
+                amount: '10.00',
+                discount_type: 'fixed_product',
+                description: 'description',
+                code
+            }
+        }).then(res => {
+            expect(res.status).to.be.eql(201)
+            expect(res.body.code).to.be.eql(code)
         })
     })
 
   })
-  
